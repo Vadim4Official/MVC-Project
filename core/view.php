45f696adc20dc1329a20ee612ce24991d2fe9f66
TeacherEremin <swivels-factor0l@icloud.com>
2022-03-17 17:33:44 +0500

    Version 1.0.6 - Work

--- core/view.php
+++ core/view.php
@@ -6,24 +6,40 @@ function view($name, $arg = []) {
 
     # Функция в переменной для определения есть ли ошибка
     $isError = function($errorName, $input = false) use ($error) {
+        # Создаем переменную с ошибками или же если нет ошибок Null
         $firstError = isset($error[$errorName]) ? $error[$errorName] : null;
-        if(!isset($firstError) && !$input) return '';
+        # Если это Input, то для него создаются атрибуты
         if($input) {
+            # Если есть ошибка, записываем класс для ошибки
             $class = (isset($firstError) ? 'is-invalid' : '');
+            # Создаем переменную для хранения всех атрибутов
             $arrAttributes = [];
+            # Добавляем к атрибуту class основной класс form-control
             $arrAttributes["class"][] = "form-control";
+            # Если есть ошибки, то добавим класс из переменной $class
             $arrAttributes["class"][] = $class;
+            # Сгенерируем для aria_describedby значение динамически validationЗНАЧЕНИЕИЗПЕРЕМЕННОЙFeedback
             $arrAttributes["aria_describedby"][] = "validation{$errorName}Feedback";
 
+            # Переменная хранящая текст для внесения в input
             $attributes = '';
+            # Цикл строящий атрибуты класса
             foreach($arrAttributes as $key => $items) {
+                # Если атрибут имеет _ то меняется на -
+                # Пример: aria_describedby будет aria-describedby
                 $key = str_replace('_', '-', $key);
+                # Получаем ключ и ставим его. Пример: class='
                 $attributes .= " $key='";
+                # Добавляем значения для данного ключа
+                # Пример: form-control is-invalid
                 foreach($items as $item)
                     $attributes .= " $item";
+                # Добавляем в конце '
                 $attributes .= "'";
             }
 
+            # Отдаем значение
+            # Пример: class='form-control is-invalid' aria-describedby='validationNameFeedback'
             return $attributes;
         }
         else {
