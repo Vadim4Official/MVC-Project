977d424181cda0b798b003f7ab5022e50a2c8f40
Vadim4Official <pivovarov.03@outlook.com>
2022-03-22 13:29:04 +0500

    v1.0.0.0.0.0.4

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 const DB_HOSTNAME = 'mysqlis.radiotech.su';
-const DB_USERNAME = 'admin_test';
-const DB_PASSWORD = 'NH1989Pr82';
-const DB_DATABASE = 'admin_test';
+const DB_USERNAME = 'is342_19_19_negr';
+const DB_PASSWORD = 'k44J2KksoT';
+const DB_DATABASE = 'is342_19_negr';
 const DB_PORT = '3306'; # Default 3306
